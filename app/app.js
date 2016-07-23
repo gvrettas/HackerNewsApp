e52799cede8cfa9b625b7b9f9d348e3df7f04516
George Vrettas <gvrettas@gmail.com>
2016-07-23 13:50:29 +1000

    Remove the seed components, views and controllers.

--- app/app.js
+++ app/app.js
@@ -2,10 +2,7 @@
 
 // Declare app level module which depends on views, and components
 angular.module('myApp', [
-  'ngRoute',
-  'myApp.view1',
-  'myApp.view2',
-  'myApp.version'
+  'ngRoute'
 ]).
 config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
   $locationProvider.hashPrefix('!');
