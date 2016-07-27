416e1056ef99f3d1d772cae4d883c6bae9b47b92
George Vrettas <gvrettas@gmail.com>
2016-07-27 20:02:25 +1000

    Add gulp tasks to build the CSS, run the app, and watch for changes to all files.

---
+++ gulpfile.js
@@ -0,0 +1,24 @@
+var gulp = require('gulp');
+var browserSync = require('browser-sync');
+var sass = require('gulp-ruby-sass');
+var reload = browserSync.reload;
+
+gulp.task('sass', function() {
+    return sass('app/styles/styles.scss')
+    .pipe(gulp.dest('app/css'))
+    .pipe(reload({ stream: true }));
+});
+
+gulp.task('serve', ['sass'], function() {
+    var options = {
+        server: {
+            baseDir: 'app'
+        }
+    };
+
+    browserSync(options);
+
+    gulp.watch('app/styles/*.scss', ['sass']);
+    gulp.watch("app/**/*.html").on('change', reload);
+    gulp.watch("app/**/*.js").on('change', reload);
+});
