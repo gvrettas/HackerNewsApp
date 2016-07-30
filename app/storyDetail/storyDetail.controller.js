f7ce1fb075a7780c64dcf2d2b2dc8a9665f2235d
George Vrettas <gvrettas@gmail.com>
2016-07-30 19:33:56 +1000

    Create the UI for the comments list.

--- app/storyDetail/storyDetail.controller.js
+++ app/storyDetail/storyDetail.controller.js
@@ -12,6 +12,11 @@
         var model = this;
         model.storyDetail = {};
 
+        model.commentSortOptions = [];
+        model.commentSortOptions.push({ label: 'Newest', sortBy: '-time' });
+        model.commentSortOptions.push({ label: 'Oldest', sortBy: 'time' });
+        model.sortBy = model.commentSortOptions[0].sortBy;
+
         model.viewTopStories = viewTopStories;
         model.openUrl = commonFunctions.openUrl;
 
